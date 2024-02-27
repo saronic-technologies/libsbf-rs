6765013ca936999cf3f32b81d847b28bf4b2d6bd
gsubmarine <gouthpromo@gmail.com>
2024-02-27 15:58:55 -0600

    fix(libsbf): update do not use fields (#5)
    * fix(sbflib): cleanup do not use flags
    
    * chore(sbflib): bump version
    
    * chore(sbflib): cleaner

--- src/lib.rs
+++ src/lib.rs
@@ -251,29 +251,24 @@ pub struct QualityInd {
     pub tow: Option<u32>,
     #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) })]
     pub wnc: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U1 { None } else { Some(x) })]
-    pub n: Option<u8>,
-    #[br(map = |x| if x == DO_NOT_USE_U1 { None } else { Some(x) })]
-    pub reserved: Option<u8>,
-
-
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 0))]
+    pub n: u8,
+    pub reserved: u8,
+    #[br( if(n > 0))]
     pub indicator_1: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 1))]
+    #[br(if(n > 1))]
     pub indicator_2: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 2))]
+    #[br(if(n > 2))]
     pub indicator_3: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 3))]
+    #[br(if(n > 3))]
     pub indicator_4: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 4))]
+    #[br(if(n > 4))]
     pub indicator_5: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 5))]
+    #[br(if(n > 5))]
     pub indicator_6: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 6))]
+    #[br(if(n > 6))]
     pub indicator_7: Option<u16>,
-    #[br(map = |x| if x == DO_NOT_USE_U2 { None } else { Some(x) }, if(n.unwrap() > 7))]
+    #[br( if(n > 7))]
     pub indicator_8: Option<u16>,
-
 }
 
 #[binrw]
