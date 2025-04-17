aed914524fbddca827af5c5f4a6cb3962f197499
Rishi Desai <rishi@saronic.com>
2025-04-17 16:27:53 -0500

    feat: add tracing logs

--- examples/read_sbf.rs
+++ examples/read_sbf.rs
@@ -3,7 +3,15 @@ use libsbf::reader::SbfReader;
 use std::env;
 use std::net::TcpStream;
 
+use tracing_subscriber::EnvFilter;
+
+
 fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .init();
+
     let ip_port = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".into());
