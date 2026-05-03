d462d43879d763a4a5e1ef867195c37ea9e45a9e
Joseph Fox-Rabinovitz <joseph.fox-rabinovitz@saronic.com>
2026-05-03 05:37:02 +0000

    safeguard to ensure logs show up

--- examples/summary.rs
+++ examples/summary.rs
@@ -180,7 +180,9 @@ fn run(
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
+        .with_env_filter(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
+        )
         .with_writer(io::stderr)
         .init();
 
